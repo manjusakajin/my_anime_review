74285c59052d729a0721a345e6d4bdbdeca3c6fe
manjusaka <sakurano96@gmail.com>
2017-05-09 18:21:08 +0700

    static_pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-
   scope "(:locale)", locale:  /en|vi/, defaults: {locale: "en"} do
     root "my_anime_list#show", page: "home"
     get "my_anime_list/:page", to: "my_anime_list#show"
+    get  "/signup",  to: "users#new"
+    post "/signup",  to: "users#create"
+    resources :users
   end
-
 end
