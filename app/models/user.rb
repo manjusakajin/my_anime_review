764b17ddb1b8cb2a621c98b73fcdf87363b3bd03
H2V <h2v9696@gmail.com>
2017-05-14 09:50:46 +0700

    Login
    Fix
    
    Fix
    
    Fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+  attr_accessor :remember_token
+
+  before_save :downcase_email
+
   has_many :reviews
   has_many :comments
   has_many :rates
@@ -10,5 +14,44 @@ class User < ApplicationRecord
     foreign_key: :followed_id
   has_many :followers, through: :passive_relationships, source: :follower
 
+  VALID_EMAIL_REGEX = /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
+
+  validates :name, presence: true, length: {minimum: 6, maximum: 50}
+  validates :email, presence: true, length: {maximum: 255},
+    format: {with: VALID_EMAIL_REGEX}, uniqueness: {case_sensitive: false}
+  validates :password, presence: true, length: {minimum:6}, allow_blank: true
+
   has_secure_password
+
+  class << self
+    def digest string
+      cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+        BCrypt::Engine.cost
+      BCrypt::Password.create string, cost: cost
+    end
+
+    def new_token
+      SecureRandom.urlsafe_base64
+    end
+  end
+
+  def remember
+    self.remember_token = User.new_token
+    update_attributes remember_digest: User.digest(remember_token)
+  end
+
+  def authenticated? remember_token
+    return false if remember_digest.nil?
+    BCrypt::Password.new(remember_digest).is_password? remember_token
+  end
+
+  def forget
+    update_attributes remember_digest: nil
+  end
+
+  private
+
+  def downcase_email
+    email.downcase!
+  end
 end
