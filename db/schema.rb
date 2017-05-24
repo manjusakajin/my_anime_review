e468d1e864b0385b9d01a7d182c3d69119c86f67
manjusaka <sakurano96@gmail.com>
2017-05-24 19:47:45 +0700

    Genre

--- db/schema.rb
+++ db/schema.rb
@@ -89,8 +89,8 @@ ActiveRecord::Schema.define(version: 20170517184238) do
     t.boolean  "is_admin"
     t.datetime "created_at",        null: false
     t.datetime "updated_at",        null: false
-    t.string   "remember_digest"
     t.string   "password_digest"
+    t.string   "remember_digest"
     t.string   "activation_digest"
     t.boolean  "is_activated"
     t.datetime "activated_at"
