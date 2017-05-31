61fd004e946e2141b7393a40c732b31396efb081
manjusaka <sakurano96@gmail.com>
2017-05-31 13:30:17 +0700

    dieu chinh giao dien

--- app/controllers/my_anime_list_controller.rb
+++ app/controllers/my_anime_list_controller.rb
@@ -1,6 +1,12 @@
 class MyAnimeListController < ApplicationController
   def show
     if valid_page?
+      if params[:page] == "home"
+        @animes = Anime.all.order(created_at: :desc)
+          .paginate page: params[:page_number], per_page: Settings.home_perpage
+        @reviews = Review.all.order(created_at: :desc)
+          .paginate page: params[:page_number], per_page: Settings.home_perpage
+      end
       render template: "my_anime_list/#{params[:page]}"
     else
       render file: "public/404.html", status: :not_found
